d109d01e55c7136193628f106b9d9e06118c920f
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-14 13:45:00 +0300

    Basic Syntax, Conditional Statements and Loops

--- 23. Reverse String.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    console.log(newString);
-}
-reverseString('SoftUni');
