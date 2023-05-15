7ba1c4d6eeb55061013ca0c02c4de5c6a1d21856
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-15 13:33:26 +0300

    Update 13. Max Numbers.js

--- 03. Arrays/13. Max Numbers.js
+++ 03. Arrays/13. Max Numbers.js
@@ -1,18 +1,18 @@
 function maxNums(arr) {
     let newArr = [];
-  for (let i = 0; i <= arr.length; i++) {
-    let curNum = arr[i];
-    let bigNum = true;
-    for(let j = i+1;j<arr.length;j++){
-        let curNum2 = arr[j];
-        if(curNum <= curNum2){
-            bigNum = false;
+    for (let i = 0; i <= arr.length; i++) {
+        let curNum = arr[i];
+        let bigNum = true;
+        for (let j = i + 1; j < arr.length; j++) {
+            let curNum2 = arr[j];
+            if (curNum <= curNum2) {
+                bigNum = false;
+            }
+        }
+        if (bigNum) {
+            newArr.push(curNum)
         }
     }
-    if(bigNum){
-        newArr.push(curNum)
-    }
-  }
-  console.log(newArr.join(" "));
+    console.log(newArr.join(" "));
 }
 maxNums([1, 4, 3, 2]);
