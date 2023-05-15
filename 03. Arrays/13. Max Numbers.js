78142616a47f9cb097991515b51f7d6494451a23
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-15 13:31:36 +0300

    03. Arrays

---
+++ 03. Arrays/13. Max Numbers.js
@@ -0,0 +1,18 @@
+function maxNums(arr) {
+    let newArr = [];
+  for (let i = 0; i <= arr.length; i++) {
+    let curNum = arr[i];
+    let bigNum = true;
+    for(let j = i+1;j<arr.length;j++){
+        let curNum2 = arr[j];
+        if(curNum <= curNum2){
+            bigNum = false;
+        }
+    }
+    if(bigNum){
+        newArr.push(curNum)
+    }
+  }
+  console.log(newArr.join(" "));
+}
+maxNums([1, 4, 3, 2]);
