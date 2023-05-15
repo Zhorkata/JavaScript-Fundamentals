f131c6544303652a1363826c5fc7972090de0c3a
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-15 13:21:43 +0300

    03. Arrays

---
+++ 03. Arrays/12. Array Rotation.js
@@ -0,0 +1,24 @@
+function rotation(arr, rotCount) {
+  while (rotCount > 0) {
+    arr.push(arr[0]);
+    arr.shift();
+    rotCount--;
+  }
+  console.log(arr.join(" "));
+}
+rotation([51, 47, 32, 61, 21], 2);
+
+function arrayRotation(arr, rotationCount) {
+  let buffArr = [];
+  for (let i = 0; i < rotationCount; i++) {
+    let currentElement = arr[0];
+    for (let j = 1; j < arr.length; j++) {
+      buffArr.push(arr[j]);
+    }
+    buffArr.push(currentElement);
+    arr = buffArr;
+    buffArr = [];
+  }
+  console.log(arr.join(" "));
+}
+arrayRotation([51, 47, 32, 61, 21], 2);
