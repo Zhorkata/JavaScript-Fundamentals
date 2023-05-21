1bb7430006ad60e0e8678d7c3f5c8696fea6afde
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-21 09:52:11 +0300

    Add files via upload

---
+++ 04. Functions/04. Orders.js
@@ -0,0 +1,12 @@
+function orders(product, quantity) {
+    let price = 0;
+    switch (product) {
+        case 'coffee': price = 1.5; break;
+        case 'coke': price = 1.4; break;
+        case 'water': price = 1; break;
+        case 'snacks': price = 2; break;
+    }
+    console.log((price * quantity).toFixed(2));
+}
+orders("water", 5)
+orders("coffee", 2)
