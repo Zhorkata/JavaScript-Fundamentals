95505d98526875b46972016a232c8558174259db
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-26 14:49:57 +0300

    Update 27. Triangle Area.js

--- 02. Data Types and Variables/27. Triangle Area.js
+++ 02. Data Types and Variables/27. Triangle Area.js
@@ -1,6 +1,13 @@
-function area(a, b, c) {
-    let result = Math.sqrt((a + b + c) * (-a + b + c) * (a - b + c) * (a + b - c)) / 4;
-    console.log(result);
-} 
-area(2, 3.5, 4)
-area(3, 5.5, 4)
+function calculateTriangleArea(side1, side2, side3) {
+  // Use Heron's formula to calculate the area
+  let semiPerimeter = (side1 + side2 + side3) / 2;
+  let area = Math.sqrt(
+    semiPerimeter *
+      (semiPerimeter - side1) *
+      (semiPerimeter - side2) *
+      (semiPerimeter - side3)
+  );
+   console.log(area);
+}
+calculateTriangleArea(2, 3.5, 4)
+
