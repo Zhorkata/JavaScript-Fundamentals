a69461de74cc942ea2b0c1d795b709c9b2e64ef2
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-27 10:03:37 +0300

    Delete Arrays Advanced directory

--- Arrays Advanced/01. Sum First and Last.js
+++
@@ -1,9 +0,0 @@
-function sumFirstAndLast(numbers) {
-
-    let firstNum = Number(numbers.shift());
-    let LastNum = Number(numbers.pop());
-    let result = firstNum + LastNum;
-    console.log(result);
-}
-// sumFirstAndLast(['20', '30', '40'])
-sumFirstAndLast(['5', '10'])
