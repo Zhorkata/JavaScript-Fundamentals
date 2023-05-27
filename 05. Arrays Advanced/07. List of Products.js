cf4dece5e76139396a3b8d74aa598175a7ef0a31
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-05-27 10:18:49 +0300

    Add files via upload

---
+++ 05. Arrays Advanced/07. List of Products.js
@@ -0,0 +1,8 @@
+function listOfProducts(products){
+    let sorted = products.sort();
+    for (let i = 0; i < products.length; i++) {
+        console.log(`${i+1}.${sorted[i]}`);
+    }
+}
+listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples'])
+listOfProducts(['Watermelon', 'Banana', 'Apples'])
