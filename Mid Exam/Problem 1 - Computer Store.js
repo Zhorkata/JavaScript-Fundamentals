f5d4260d15bdb567f344f33f85bbb9600d77877b
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-02 15:39:40 +0300

    Update Problem 1 - Computer Store.js

--- Mid Exam/Problem 1 - Computer Store.js
+++ Mid Exam/Problem 1 - Computer Store.js
@@ -1,47 +1,47 @@
 function computerStore(numbers) {
     let numLen = numbers.length;
-
     let priceWithoutTaxes = 0;
     let totalPrice = 0;
     let taxes = 0;
-    for (let i = 0; i < numLen; i++) {
+    let isSpecial = false;
 
-        let currentPrice = Number(numbers[i]);//1050
+    for (let i = 0; i < numLen; i++) {
+        let currentPrice = Number(numbers[i]);
 
         if (currentPrice <= 0) {
             console.log('Invalid price!');
             continue;
         }
 
-        if (numbers[i] == 'special' || numbers[i] == 'regular') {
-            if (numbers[i] == 'special') {
+        if (numbers[i] === 'special' || numbers[i] === 'regular') {
+            if (numbers[i] === 'special') {
+                isSpecial = true;
+            }
+
+            if (totalPrice === 0) {
+                console.log('Invalid order!');
+                break;
+            }
+
+            console.log('Congratulations you\'ve just bought a new computer!');
+            console.log(`Price without taxes: ${priceWithoutTaxes.toFixed(2)}$`);
+            console.log(`Taxes: ${taxes.toFixed(2)}$`);
+            console.log('-----------');
+
+            if (isSpecial) {
                 totalPrice *= 0.9;
-               
-                console.log('Congratulations you\'ve just bought a new computer!');
-                console.log(`Price without taxes: ${priceWithoutTaxes.toFixed(2)}$`);
-                console.log(`Taxes: ${taxes.toFixed(2)}$`);
-                console.log('-----------');
-                console.log(`Total price: ${totalPrice.toFixed(2)}$`);
             }
-            else{
-                if(totalPrice === 0){
-                    console.log('Invalid order!');
-                    break;
-            }
-                console.log('Congratulations you\'ve just bought a new computer!');
-                console.log(`Price without taxes: ${priceWithoutTaxes.toFixed(2)}$`);
-                console.log(`Taxes: ${taxes.toFixed(2)}$`);
-                console.log('-----------');
-                console.log(`Total price: ${totalPrice.toFixed(2)}$`);
-            }
+
+            console.log(`Total price: ${totalPrice.toFixed(2)}$`);
             break;
         }
 
-        priceWithoutTaxes += currentPrice;//1023...1544.96
-        taxes += currentPrice * 0.2;//204.6...308.992
-        totalPrice = priceWithoutTaxes + taxes;//1227.6...1853.95
+        priceWithoutTaxes += currentPrice;
+        taxes += currentPrice * 0.2;
+        totalPrice = priceWithoutTaxes + taxes;
     }
 }
+
 computerStore([
     '1050',
     '200',
@@ -50,27 +50,25 @@ computerStore([
     '18.50',
     '16.86',
     'special'
-])
+]);
+
 console.log('===============Another output=================');
+
 computerStore([
-    '1023', 
-    '15', 
+    '1023',
+    '15',
     '-20',
     '-5.50',
-    '450', 
-    '20', 
-    '17.66', 
-    '19.30', 
+    '450',
+    '20',
+    '17.66',
+    '19.30',
     'regular'
-    ])
+]);
+
 console.log('==============Another output==================');
+
 computerStore([
-'regular'
-])
-
-
-    
-
-
-
+    'regular'
+]);
 
