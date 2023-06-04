601b3f81d7894d8f1d1b352ab451716ba679e072
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-04 11:54:50 +0300

    Update 09. Train.js

--- 05. Arrays Advanced/09. Train.js
+++ 05. Arrays Advanced/09. Train.js
@@ -1,32 +1,3 @@
-// function train(array) {
-//     // let passengersSeats = array.shift();
-//     // let maxCapacity = array.shift();
-//     let passengersSeats = array.slice(0, 1);
-//     let maxCapacity = array.slice(1, 2);
-
-//     for (let i = 2; i < array.length; i++) {
-
-//         let command = array[i];//'Add 10'
-//         command = command.split(' ');//['Add', "10"]
-
-
-//         if (command.includes('Add')) {
-//             passengersSeats.push(command[1])
-//         }else{
-//             passengersSeats[0] = Number(passengersSeats[0]) + Number(command[0])
-
-//         }
-//         console.log(passengersSeats);//72 54 21 12 4 75 23 10 0
-//     }
-// }
-// train(['32 54 21 12 4 0 23',
-//     '75',
-//     'Add 10',
-//     'Add 0',
-//     '30',
-//     '10',
-//     '75'])
-
 function train(input) {
     let wagons = input.shift().split(" ").map(Number);
     let maxCapacity = Number(input.shift());
