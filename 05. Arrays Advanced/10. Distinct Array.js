21b155c978ed684e113f062aaedf2771b9c0f282
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-04 11:56:47 +0300

    Update 10. Distinct Array.js

--- 05. Arrays Advanced/10. Distinct Array.js
+++ 05. Arrays Advanced/10. Distinct Array.js
@@ -1,15 +1,14 @@
 function distinctArray(array) {
     let copyArray = array.slice(0);
     let newArr = [];
+    
     for (let i = 0; i < array.length; i++) {
         let temp = copyArray[i];
         if (!newArr.includes(temp)) {
             newArr.push(temp)
-            //console.log(copyArray[i]);
         }
     }
     console.log(newArr.join(' '));
-
 }
 distinctArray([1, 2, 3, 4])
 distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2])
