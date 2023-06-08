b777cb4d95bd9bd2e266472ce526d9b6e418685f
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-08 12:12:22 +0300

    Update SoftUni Reception.js

--- Mid Exam/SoftUni Reception.js
+++ Mid Exam/SoftUni Reception.js
@@ -19,12 +19,8 @@ function reception(array) {
 
             break;
         }
-
-      
     }
-
 }
-
 reception(['5', '6', '4', '20'])
 reception(['1', '2', '3', '45'])
 reception(['3', '2', '5', '40'])
