2470766ba411baf3eb333feb5014c0b3de60d630
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-11 14:31:52 +0300

    Update Problem 2 - Array Modifier.js

--- Mid Exam/Problem 2 - Array Modifier.js
+++ Mid Exam/Problem 2 - Array Modifier.js
@@ -1,12 +1,29 @@
-function arrayModifier(array){
+function arrayModifier(array) {
     let values = array.shift().split(' ');
-    let row = array[0].split(' ');
-    let command = row[0];
+    let row = array[0].split(' ');//swap , 1 , 3...
+    let command = row[0];//swap...
     console.log(values);
     console.log(row);
     console.log(command);
+    while (command !== 'end') {
+        switch (command) {
+            case 'swap':
+
+                break;
+
+            case 'multiply':
+                //current index num multiply by the other
+
+                break;
+            case 'decrease':
+                //all nums - 1
+
+                break;
+        }
+    }
 
 }
+
 arrayModifier([
     '23 -2 321 87 42 90 -123',
     'swap 1 3',
@@ -18,11 +35,11 @@ arrayModifier([
     'end'
 ])
 arrayModifier([
-        '1 2 3 4',
-        'swap 0 1',
-        'swap 1 2',
-        'swap 2 3',
-        'multiply 1 2',
-        'decrease',
-        'end'
+    '1 2 3 4',
+    'swap 0 1',
+    'swap 1 2',
+    'swap 2 3',
+    'multiply 1 2',
+    'decrease',
+    'end'
 ])
