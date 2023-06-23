bbe6a4af137efd1acf5ae38b861b4f3e7c82784b
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-23 15:21:25 +0300

    Update 08. Towns.js

--- 06. Objects and Classes/08. Towns.js
+++ 06. Objects and Classes/08. Towns.js
@@ -1,10 +1,16 @@
 function towns(data){
 
-    let object = {};
-    data.forEach(row => {row.split(' , ')
-        console.log(row[0]);
-    });
+    data.forEach(row => {
+        row.split(' , ')
+        let [townName, latitude, longitude] = row.split(' | ');
 
+        let object = {
+            town: townName,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2)
+        }
+        console.log(object);
+    });
 }
 towns(['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625'])
