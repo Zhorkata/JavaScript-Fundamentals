04069a9925ae361c2d450e700883b8b8f3d8a570
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-24 20:54:28 +0300

    Update 10. Movies.js

--- 06. Objects and Classes/10. Movies.js
+++ 06. Objects and Classes/10. Movies.js
@@ -1,15 +1,40 @@
-function movies(data){
-    
-    let array = [];
-    for (let i = 0; i < data.length; i++) {
-        let row = data[0];
-        if(row.includes('addMovie')){
+function movies(data) {
 
-            array.push()
+    let films = [];
+
+    data.forEach(row => {
+
+        if (row.includes('addMovie')) {
+
+            let movieName = row.split('addMovie ')[1];
+            films.push({ name: movieName });
+
+        } else if (row.includes('directedBy')) {
+
+            let [name, director] = row.split(' directedBy ');
+
+            let movie = films.find(m => m.name === name);
+
+            if (movie) {
+                movie.director = director;
+            }
+
+        } else if (row.includes('onDate')) {
+
+            let [name, date] = row.split(' onDate ');
+            let movie = films.find(m => m.name === name);
+
+            if (movie) {
+                movie.date = date;
+            }
         }
-        
-    }
+    });
 
+    films.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    })
 }
 movies([
     'addMovie Fast and Furious',
@@ -20,4 +45,13 @@ movies([
     'Fast and Furious onDate 30.07.2018',
     'Batman onDate 01.08.2018',
     'Fast and Furious directedBy Rob Cohen'
-    ])
+])
+console.log(`--------CASE-----`)
+movies([
+    'addMovie The Avengers',
+    'addMovie Superman',
+    'The Avengers directedBy Anthony Russo',
+    'The Avengers onDate 30.07.2010',
+    'Captain America onDate 30.07.2010',
+    'Captain America directedBy Joe Russo'
+])
