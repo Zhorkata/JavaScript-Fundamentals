58a5dfc3a358f1b3143acdcc15c66df9c178937c
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-24 18:34:01 +0300

    Update 10. Movies.js

---
+++ 06. Objects and Classes/10. Movies.js
@@ -0,0 +1,23 @@
+function movies(data){
+    
+    let array = [];
+    for (let i = 0; i < data.length; i++) {
+        let row = data[0];
+        if(row.includes('addMovie')){
+
+            array.push()
+        }
+        
+    }
+
+}
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+    ])
