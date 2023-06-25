a6a0b4363afe89a688516e3c20c2cde423223404
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-06-25 20:18:48 +0300

    Update 11. Inventory.js

---
+++ 06. Objects and Classes/11. Inventory.js
@@ -0,0 +1,19 @@
+function inventory(array){
+
+    let levelsAsArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let [heroName, heroLevel, item] = array[i].split(' / ');
+        levelsAsArray.push(heroLevel);
+        
+        sortedLevels = levelsAsArray.sort((a,b)=> a-b);
+     
+        console.log(`Hero: ${heroName} \nlevel => ${heroLevel} \nitems => ${item}`);
+    }
+    
+}
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ])
