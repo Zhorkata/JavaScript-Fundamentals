c50be7f9b9ff9cf96203e4982c03ed035d26d8e1
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-02 16:04:42 +0300

    Update 11. Inventory.js

--- 06. Objects and Classes/11. Inventory.js
+++ 06. Objects and Classes/11. Inventory.js
@@ -1,17 +1,36 @@
-function inventory(array){
-    let levelsAsArray = [];
-    
+function inventory(array) {
+    let heroes = [];
+
     for (let i = 0; i < array.length; i++) {
-        let [heroName, heroLevel, item] = array[i].split(' / ');
-        levelsAsArray.push(heroLevel);
-        
-        sortedLevels = levelsAsArray.sort((a,b)=> a-b);
-     
-        console.log(`Hero: ${heroName} \nlevel => ${heroLevel} \nitems => ${item}`);
+        let [heroName, heroLevel, items] = array[i].split(' / ');//split each element
+        let itemArray = items.split(', ');//add each item in array
+
+        heroes.push({
+            name: heroName,
+            level: Number(heroLevel),
+            items: itemArray,
+        });
+    }
+
+    heroes.sort((a, b) => a.level - b.level);
+
+    for (let i = 0; i < heroes.length; i++) {
+        let { name, level, items } = heroes[i];
+        let itemsString = items.join(', ');
+
+        console.log(`Hero: ${name}`);
+        console.log(`level => ${level}`);
+        console.log(`items => ${itemsString}`);
     }
 }
 inventory([
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
     'Hes / 1 / Desolator, Sentinel, Antara'
-    ])
+])
+console.log(`-------------case------------`);
+inventory([
+    'Batman / 2 / Banana, Gun',
+    'Superman / 18 / Sword',
+    'Poppy / 28 / Sentinel, Antara'
+])
