359cf7e428a9519ad57e49601d6e6c69b6927cde
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-10 22:11:41 +0300

    Update 06. Word Occurrences.js

---
+++ 07. Associative Arrays/06. Word Occurrences.js
@@ -0,0 +1,14 @@
+function wordOccurences(input) {
+    let object = {};
+    let map = new Map();
+    for (const word of input) {
+        
+        
+    }
+
+}
+wordOccurences([
+    "Here", "is", "the", "first", "sentence",
+    "Here", "is", "another", "sentence", "And",
+    "finally", "the", "third", "sentence"
+])
