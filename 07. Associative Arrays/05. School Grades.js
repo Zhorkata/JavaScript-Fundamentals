bd591948fda6059310b052d7ed95ed03c2eb0ed0
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-10 21:12:42 +0300

    Update 05. School Grades.js

--- 07. Associative Arrays/05. School Grades.js
+++ 07. Associative Arrays/05. School Grades.js
@@ -4,16 +4,37 @@ function schoolGrades(input) {
     for (const line of input) {
         let entry = line.split(" ");
         let name = entry[0];
-        let grades = entry.slice(1).map(Number).join();
+        let grades = entry.slice(1).map(Number);
 
         if (gradesList.hasOwnProperty(name)) {
             gradesList[name] = gradesList[name].concat(grades);
         } else {
             gradesList[name] = grades;
         }
-
     }
 
+    let studentAverages = {};
+
+    // Calculate average grades for each student
+    for (const name in gradesList) {
+        let grades = gradesList[name];
+        let sum = 0;
+
+        for (const grade of grades) {
+            sum += grade;
+        }
+
+        let average = (sum / grades.length).toFixed(2);
+        studentAverages[name] = parseFloat(average);
+    }
+
+    // Sort student averages alphabetically by name
+    let sortedAverages = Object.entries(studentAverages).sort();
+
+    // Print student names and average grades
+    for (const [name, average] of sortedAverages) {
+        console.log(`${name}: ${average.toFixed(2)}`);
+    }
 }
 schoolGrades([
     'Lilly 4 6 6 5',
