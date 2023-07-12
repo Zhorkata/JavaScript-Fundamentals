5e94503b484af12b1073492f829b81a4ddb865ec
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-12 22:14:11 +0300

    Update 08. Odd Occurrences.js

--- 07. Associative Arrays/08. Odd Occurrences.js
+++ 07. Associative Arrays/08. Odd Occurrences.js
@@ -1,19 +1,17 @@
 function oddOccurrences(input){
     let words = input.split(' ');
     let oddAppears = false;
+    let appearrenceCount = 0;
+
     for (let i = 0; i < input.length; i++) {
         if(words.includes(words[i])){
-            words[i] = words[i] ? words[i] : false ;
+            appearrenceCount++;
+
+            if(appearrenceCount % 2 !== 0){
+                oddAppears = true;
+                console.log(words[i]);
+            }
         }
-        
-        
     }
-    if(oddAppears){
-        console.log(words[i]);
-    }
-    // console.log(words[0]);
 }
 oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
-// if (myWords.includes(word)) {
-//     wordOccurrences[word] = wordOccurrences[word] ? wordOccurrences[word] + 1 : 1;
-// }
