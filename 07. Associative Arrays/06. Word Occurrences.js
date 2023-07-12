dd402ea6b0da4ec270669e689487632323fc4591
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-12 20:44:12 +0300

    Update 06. Word Occurrences.js

--- 07. Associative Arrays/06. Word Occurrences.js
+++ 07. Associative Arrays/06. Word Occurrences.js
@@ -1,20 +1,23 @@
-function wordOccurences(input) {
+function wordOccurrences(input) {
     let object = {};
-    let map = new Map();
 
     for (const word of input) {
         object[word] = object[word] ? object[word] + 1 : 1;
     }
 
-    let sortedDescending = Array.from(map).sort((a,b)=> b[1] - a[1]);
-    
-    for (const key in object) {
-       console.log(`${key} -> ${object[key]} times`);
-    }
+    let sortedDescending = Object.entries(object).sort((a, b) => b[1] - a[1]);
 
+    for (const [key, value] of sortedDescending) {
+        console.log(`${key} -> ${value} times`);
+    }
 }
-wordOccurences([
+wordOccurrences([
     "Here", "is", "the", "first", "sentence",
     "Here", "is", "another", "sentence", "And",
     "finally", "the", "third", "sentence"
+]);
+console.log(`-----------CASE 2---------`);
+wordOccurrences([
+    "dog", "bye", "city", "dog", "dad",
+    "boys", "ginger"
 ])
