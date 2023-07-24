e608d2d10a39dd1c02a23852b1ae5a6cb0a31911
Georgi Petkov <116912227+Zhorkata@users.noreply.github.com>
2023-07-24 09:23:01 +0300

    Update 10. Pascal-Case Splitter.js

---
+++ 08. Text Processing/10. Pascal-Case Splitter.js
@@ -0,0 +1,16 @@
+function splitter(string){
+
+    let lowerCased = string.toLowerCase();
+    let output = '';
+    for (let i = 0; i < string.length; i++) {
+        if(lowerCased[i] === string[i]){
+            //here we continue with writing
+        }else{
+            //we need to split
+            
+        }
+    }
+   console.log(output);
+
+}
+splitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
